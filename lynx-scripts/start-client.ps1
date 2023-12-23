f5f55bc69407d88c7e2afee1e3fe1e4e258ba63d
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2023-12-24 00:05:17 +0100

    Add -u to client ps1 script to allow output redirect

--- lynx-scripts/start-client.ps1
+++ lynx-scripts/start-client.ps1
@@ -1,8 +1,8 @@
-python3 OpenBench/Client/client.py `
+python3 -u OpenBench/Client/client.py `
 	-S https://openbench.lynx-chess.com/ `
 	-U "<user>" `
 	-P "<pwd>" `
 	-T 8 `
 	-N 1 `
-	-I mini-worker-1
+	-I mini-worker-1 `
 | tee ob-client.txt
