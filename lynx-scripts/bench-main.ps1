05c46e6d6930e757469ba08b79adfa43aa9ba84d
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-10-06 22:37:20 +0200

    Add `--rebuild` option to `bench-main.py`

--- lynx-scripts/bench-main.ps1
+++ lynx-scripts/bench-main.ps1
@@ -1,1 +1,1 @@
-python3 .\Scripts\bench_all.py -S https://openbench.lynx-chess.com/ --username eduherminio --password "<pwd>" --threads 8 --sets 1
+python3 .\Scripts\bench_all.py -S https://openbench.lynx-chess.com/ --username eduherminio --password "<pwd>" --rebuild --threads 8 --sets 5
