7471afc663120e10bbdd22612d5af08dc7f6e61f
Eduardo Cáceres <eduherminio@users.noreply.github.com>
2025-06-06 12:03:30 +0200

    Tweak Linux start-client.sh script

--- lynx-scripts/start-client.sh
+++ lynx-scripts/start-client.sh
@@ -4,9 +4,10 @@ cat /proc/cpuinfo | grep "physical id" | sort -u
 lscpu | grep "CPU(s)"
 nproc
 
-#export DOTNET_gcServer=0
+export DOTNET_gcServer=0
 
 python3 ./Client/Client.py -U eduherminio -P "<pwd>" -S https://openbench.lynx-chess.com/ -T 3 -N 1 -I ob-worker-n
+# Client/.venv/bin/python3 Client/client.py
 
 # https://vipinyadav.com/blog/scripts-run-in-background-using-screen-detached-mode/
 # Create a new screen session: screen -S <session-name>. -S is short for session the name.
@@ -14,3 +15,7 @@ python3 ./Client/Client.py -U eduherminio -P "<pwd>" -S https://openbench.lynx-c
 # To return to the virtual screen session use command screen -r.
 # To list the screen sessions run command screen -list.
 # To choose a different screen session you can use pid or session name. Run screen -r <pid-or-session-name>
+
+# cd Client
+# python3 -m venv .venv
+# .venv/bin/pip3 install -r requirements.txt
