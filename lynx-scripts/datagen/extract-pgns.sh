b5271e918e8c3686aaedd7067a1c2ec12a305ad1
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2026-07-30 19:02:31 +0200

    Add parallelism to datagen scripts

--- lynx-scripts/datagen/extract-pgns.sh
+++ lynx-scripts/datagen/extract-pgns.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 DIR="${1:-.}"
-for file in "$DIR"/*.bz2; do
-    bzip2 -d "$file"
-done
+
+# for file in "$DIR"/*.bz2; do
+#     bzip2 -d "$file"
+# done
+
+ls "$DIR"/*.bz2 | parallel bzip2 -d
 
 $(dirname "$0")/create-pgn-files-file.sh "$DIR"
--- lynx-scripts/datagen/pgn-to-vf.sh
+++ lynx-scripts/datagen/pgn-to-vf.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 DIR="${1:-.}"
-for file in "$DIR"/*.pgn; do
-    ~/dev/chess-engines/pawnocchio/pawnocchio pgntovf "$file"
-done
+
+# for file in "$DIR"/*.pgn; do
+#     ~/dev/chess-engines/pawnocchio/pawnocchio pgntovf "$file"
+# done
+
+ls "$DIR"/*.pgn | parallel ~/dev/chess-engines/pawnocchio/pawnocchio pgntovf
